6c967783987ab51ebe5285e9407aee6429d6c347
Matt Farnsworth <mryanfarnsworth@gmail.com>
2018-08-01 14:04:00 -0600

    setup logo component and corresponding styles.

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Logo from './logo';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <div>Logo</div>
+        <Logo/>
       </div>
     );
   }
---
+++ src/components/logo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Logo extends Component {
+    render() {
+        return (
+            <div className="logo-main">
+                <img src="/assets/ds_circle_logo.png" alt='A daily smarty logo'/>
+            </div>
+        );
+    }
+}
+export default Logo;
